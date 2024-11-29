dee2fb36c2eaf88c375b4515c7383588c1ff6c7c
Adhicitta Masran <adhicittamasran@gmail.com>
2024-11-30 00:57:52 +0800

    build(migration): add users table

---
+++ prisma/migrations/20241129165737_add_users_table/migration.sql
@@ -0,0 +1,23 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('ADMIN', 'SUPER', 'USER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" VARCHAR(255) NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" VARCHAR(120) NOT NULL,
+    "email" VARCHAR(120) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
