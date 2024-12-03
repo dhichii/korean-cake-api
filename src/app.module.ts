21b8a2c928cd97a4993b45ee84b8b577d68f314d
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-04 01:26:13 +0800

    feat: add validation service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { PrismaService } from './common/prisma.service';
+import { ValidationService } from './common/validation.service';
 
 @Module({
   imports: [CommonModule],
   controllers: [],
-  providers: [PrismaService],
-  exports: [PrismaService],
+  providers: [PrismaService, ValidationService],
+  exports: [PrismaService, ValidationService],
 })
 export class AppModule {}
