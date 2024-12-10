a8b73232ffb5790ba47e81630a8051486faa220b
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-10 23:53:42 +0800

    feat(user): add repo interface

---
+++ src/user/domain/user.repository.interface.ts
@@ -0,0 +1,13 @@
+import { UserEntity } from './user.entity';
+
+export interface IUserRepository {
+  add(data: UserEntity): Promise<string>;
+  getAll(): Promise<UserEntity[]>;
+  getById(id: string): Promise<UserEntity>;
+  getByUsername(username: string): Promise<UserEntity>;
+  changeEmail(id: string, email: string): Promise<void>;
+  changeUsername(id: string, username: string): Promise<void>;
+  changePassword(id: string, password: string): Promise<void>;
+  deleteById(id: string): Promise<void>;
+  verify(id: string): Promise<void>;
+}
