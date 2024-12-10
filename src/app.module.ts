64d7c8b841f6252c0207a6db74f27716bb451e49
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-10 22:55:41 +0800

    feat(user): add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from './common/common.module';
 import { PrismaService } from './common/prisma.service';
 import { ValidationService } from './common/validation.service';
 import { AppController } from './app.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, UserModule],
   controllers: [AppController],
   providers: [PrismaService, ValidationService],
   exports: [PrismaService, ValidationService],
