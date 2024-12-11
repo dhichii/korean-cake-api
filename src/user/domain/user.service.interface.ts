d8b6ecf8fedf788084f4e94454a689e27c67fe5d
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-11 11:06:52 +0800

    feat(user): add service interface

---
+++ src/user/domain/user.service.interface.ts
@@ -0,0 +1,16 @@
+import { AddUserReq } from '../interface/http/user.request';
+import {
+  UserResponse,
+  UserWithPasswordResponse,
+} from '../interface/http/user.response';
+
+export interface IUserService {
+  add(req: AddUserReq): Promise<{ id: string }>;
+  getAll(): Promise<UserResponse[]>;
+  getById(id: string): Promise<UserResponse>;
+  getByUsername(username: string): Promise<UserWithPasswordResponse>;
+  deleteById(id: string): Promise<void>;
+  changeEmail(id: string, email: string): Promise<void>;
+  changeUsername(id: string, username: string): Promise<void>;
+  changePassword(id: string, password: string): Promise<void>;
+}
