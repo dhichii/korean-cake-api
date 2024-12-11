ef3002838b7a9c0c919ac5e1b68e4d07c51f91ff
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-11 21:43:53 +0800

    feat: implement OnModuleInit in PrismaService

--- src/common/prisma.service.ts
+++ src/common/prisma.service.ts
@@ -1,13 +1,13 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
 import { Prisma, PrismaClient } from '@prisma/client';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 
 @Injectable()
-export class PrismaService extends PrismaClient<
-  Prisma.PrismaClientOptions,
-  string
-> {
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, string>
+  implements OnModuleInit
+{
   constructor(
     @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
   ) {
