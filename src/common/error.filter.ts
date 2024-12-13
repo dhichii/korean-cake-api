088f6752d5c730f8fdd2e9346aa7891d85b13cce
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-13 20:50:42 +0800

    fix: can't handle HttpException
    use instanceof to handle extended class

--- src/common/error.filter.ts
+++ src/common/error.filter.ts
@@ -11,16 +11,17 @@ import { ZodError } from 'zod';
 export class ErrorFilter implements ExceptionFilter {
   catch(exception: any, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse();
-    switch (exception.constructor) {
-      case HttpException:
+    switch (true) {
+      case exception instanceof HttpException:
+        const { message } = exception.getResponse() as HttpException;
         return response.status(exception.getStatus()).json({
-          errors: [{ message: exception.getResponse() }],
+          errors: [{ message }],
         });
-      case ZodError:
+      case exception instanceof ZodError:
         return response.status(400).json({
           errors: JSON.parse(exception.message),
         });
-      case Prisma.PrismaClientKnownRequestError:
+      case exception instanceof Prisma.PrismaClientKnownRequestError:
         if (exception.code === 'P2002') {
           return response.status(400).json({
             errors: [{ message: `${exception.meta?.target} is already exist` }],
