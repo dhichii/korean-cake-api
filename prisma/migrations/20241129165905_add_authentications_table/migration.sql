edf762d4584066ff8472e30309fc70feddb73c7d
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-18 23:09:53 +0800

    build(migration): add revoked column to the authentications table

--- prisma/migrations/20241129165905_add_authentications_table/migration.sql
+++ prisma/migrations/20241129165905_add_authentications_table/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "authentications" (
     "token" VARCHAR(300) NOT NULL,
     "expiresAt" TIMESTAMP(3) NOT NULL,
+    "revoked" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "authentications_pkey" PRIMARY KEY ("token")
 );
