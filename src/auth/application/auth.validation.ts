071f70da67382777a8d579ee2c8554de64e58580
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-21 23:58:43 +0800

    feat(auth): add usecase validation

---
+++ src/auth/application/auth.validation.ts
@@ -0,0 +1,22 @@
+import { USERNAME_REGEX } from 'src/utils/regex';
+import { z, ZodType } from 'zod';
+
+export class AuthValidation {
+  static readonly REGISTER: ZodType = z.object({
+    name: z.string().min(1),
+    username: z
+      .string()
+      .min(3)
+      .regex(USERNAME_REGEX, 'username can only be letters and numbers.'),
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
+
+  static readonly LOGIN: ZodType = z.object({
+    username: z
+      .string()
+      .min(3)
+      .regex(USERNAME_REGEX, 'username can only be letters and numbers.'),
+    password: z.string().min(8),
+  });
+}
