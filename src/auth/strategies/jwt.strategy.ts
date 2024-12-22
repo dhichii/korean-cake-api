cd2d04c65eb4ed34eb23522e8d0b8e749153a56e
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-22 22:43:04 +0800

    feat(auth): add local, jwt, and refresh-jwt strategy

---
+++ src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JWTSignPayload } from '../interface/http/auth.response';
+
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.ACCESS_TOKEN_KEY,
+    });
+  }
+
+  async validate(payload: JWTSignPayload) {
+    return {
+      id: payload.id,
+      name: payload.name,
+      username: payload.username,
+      email: payload.email,
+      role: payload.role,
+      createdAt: payload.createdAt,
+      updatedAt: payload.updatedAt,
+    };
+  }
+}
