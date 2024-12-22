79e7490a92e9fe62b1f027da3c966b6dc30a305a
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-22 22:44:56 +0800

    feat(auth): register module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaService } from './common/prisma.service';
 import { ValidationService } from './common/validation.service';
 import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CommonModule, UserModule],
+  imports: [CommonModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [PrismaService, ValidationService],
   exports: [PrismaService, ValidationService],
