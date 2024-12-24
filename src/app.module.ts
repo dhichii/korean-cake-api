1da5ae3c50e7bd12adc2f37ac88eb041bfea2fda
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-24 23:50:15 +0800

    feat: add welcome in root route

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
 import { PrismaService } from './common/prisma.service';
 import { ValidationService } from './common/validation.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [CommonModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [PrismaService, ValidationService],
   exports: [PrismaService, ValidationService],
 })
