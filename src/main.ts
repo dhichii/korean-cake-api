c509faaae9a9ebfb3294ccfc3c597e29bac34a66
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-25 21:10:00 +0800

    docs(swagger): add nest swagger for API documentation generator

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
@@ -7,6 +8,16 @@ async function bootstrap() {
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
+
+  const documentConfig = new DocumentBuilder()
+    .setTitle('Korean Cake')
+    .setDescription('The Korean Cake API Documentation')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentConfig);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
