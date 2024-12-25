e91c94267825a299aba4fec55214363c361bc993
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-25 11:11:50 +0800

    fix(app): remove prisma and validation service from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { CommonModule } from './common/common.module';
-import { PrismaService } from './common/prisma.service';
-import { ValidationService } from './common/validation.service';
 import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
@@ -9,7 +7,5 @@ import { AuthModule } from './auth/auth.module';
 @Module({
   imports: [CommonModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [PrismaService, ValidationService],
-  exports: [PrismaService, ValidationService],
 })
 export class AppModule {}
