95c8afe0fc6318adafd63bf5716ec5d08ceb6020
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-26 20:58:33 +0800

    feat(admin): add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from './common/common.module';
 import { AppController } from './app.controller';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [CommonModule, UserModule, AuthModule],
+  imports: [CommonModule, UserModule, AuthModule, AdminModule],
   controllers: [AppController],
 })
 export class AppModule {}
