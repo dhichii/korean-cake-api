b01c80e4a2fbbba1e472501446cfb4d13bd135a7
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-28 00:40:46 +0800

    fix(admin): import module path should be relative

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './interface/http/admin.controller';
 import { AdminService } from './application/admin.service';
-import { UserModule } from 'src/user/user.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [UserModule],
