d9b6e2bb4b1dc8bc3704e1038dbe0295f58fd18f
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-28 21:44:36 +0800

    test(admin): fix service module creation

--- src/admin/application/admin.service.spec.ts
+++ src/admin/application/admin.service.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminService } from './admin.service';
+import { UserModule } from '../../user/user.module';
+import { CommonModule } from '../../common/common.module';
 
 describe('AdminService', () => {
   let service: AdminService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CommonModule, UserModule],
       providers: [AdminService],
     }).compile();
 
