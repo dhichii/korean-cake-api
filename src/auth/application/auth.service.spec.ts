8ec4966a400dee42c4cab535ba4055676bb3e732
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-29 18:41:40 +0800

    test(auth): fix module creation for auth service test

--- src/auth/application/auth.service.spec.ts
+++ src/auth/application/auth.service.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { CommonModule } from '../../common/common.module';
+import { AuthRepository } from '../infrastructure/auth.repository';
+import { UserModule } from '../../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        CommonModule,
+        UserModule,
+        JwtModule.register({
+          secret: process.env.ACCESS_TOKEN_KEY,
+          signOptions: { expiresIn: process.env.ACCESS_TOKEN_AGE },
+        }),
+      ],
+      providers: [
+        {
+          provide: 'IAuthRepository',
+          useClass: AuthRepository,
+        },
+        AuthService,
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
