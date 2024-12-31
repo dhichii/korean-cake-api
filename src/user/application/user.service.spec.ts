caa10d6cd7f0030f86f0f8fa4e44f9e7acc71456
Adhicitta Masran <adhicittamasran@gmail.com>
2024-12-31 23:58:11 +0800

    test(user): import auth module in service test

--- src/user/application/user.service.spec.ts
+++ src/user/application/user.service.spec.ts
@@ -2,13 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
 import { CommonModule } from '../../common/common.module';
 import { UserRepository } from '../infrastructure/user.repository';
+import { AuthModule } from '../../auth/auth.module';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [CommonModule],
+      imports: [CommonModule, AuthModule],
       providers: [
         {
           provide: 'IUserRepository',
