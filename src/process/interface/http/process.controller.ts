37c96749f10804dfd0ec89d625c3441fd5c5fbb7
Adhicitta Masran <adhicittamasran@gmail.com>
2025-01-06 21:52:58 +0800

    docs(process): add swagger spec

--- src/process/interface/http/process.controller.ts
+++ src/process/interface/http/process.controller.ts
@@ -16,7 +16,11 @@ import { JwtGuard } from '../../../auth/guards/jwt.guard';
 import { RolesGuard } from '../../../common/roles.guard';
 import {
   ApiResponseDto,
+  ForbiddenResponse,
+  InternalServerResponse,
   StatusResponseDto,
+  UnauthorizedResponse,
+  ValidationErrorResponse,
 } from '../../../common/api-response.dto';
 import { IProcessService } from 'src/process/domain/process.service.interface';
 import {
@@ -28,9 +32,23 @@ import {
   AddProcessResponseDto,
   GetAllProcessResponseDto,
 } from './process.response';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiExtraModels,
+  ApiForbiddenResponse,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+  getSchemaPath,
+} from '@nestjs/swagger';
 
 @Controller('/api/v1/process')
 @UseGuards(JwtGuard, new RolesGuard([Role.SUPER, Role.ADMIN]))
+@ApiExtraModels(ApiResponseDto, AddProcessResponseDto, GetAllProcessResponseDto)
 export class ProcessController {
   constructor(
     @Inject('IProcessService') private processService: IProcessService,
@@ -38,6 +56,39 @@ export class ProcessController {
 
   @Post()
   @HttpCode(201)
+  @ApiOperation({
+    summary: 'add new process',
+    description: 'for super/admin only',
+  })
+  @ApiBearerAuth('Authorization')
+  @ApiCreatedResponse({
+    description: 'Successfully add new process',
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(ApiResponseDto) },
+        {
+          properties: {
+            data: {
+              $ref: getSchemaPath(AddProcessResponseDto),
+            },
+          },
+        },
+      ],
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'Validation Error',
+    type: ValidationErrorResponse,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Invalid Token or Expired',
+    type: UnauthorizedResponse,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden', type: ForbiddenResponse })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    type: InternalServerResponse,
+  })
   async add(
     @Body() req: AddProcessDto,
   ): Promise<ApiResponseDto<AddProcessResponseDto>> {
@@ -48,6 +99,36 @@ export class ProcessController {
 
   @Get()
   @HttpCode(200)
+  @ApiOperation({
+    summary: 'get all processes',
+    description: 'for super/admin only',
+  })
+  @ApiBearerAuth('Authorization')
+  @ApiOkResponse({
+    description: 'Successfully get all admin',
+    schema: {
+      allOf: [
+        { $ref: getSchemaPath(ApiResponseDto) },
+        {
+          properties: {
+            data: {
+              type: 'array',
+              items: { $ref: getSchemaPath(GetAllProcessResponseDto) },
+            },
+          },
+        },
+      ],
+    },
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Invalid Token or Expired',
+    type: UnauthorizedResponse,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden', type: ForbiddenResponse })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    type: InternalServerResponse,
+  })
   async getAll(): Promise<ApiResponseDto<GetAllProcessResponseDto[]>> {
     const data = await this.processService.getAll();
 
@@ -56,6 +137,28 @@ export class ProcessController {
 
   @Put(':id')
   @HttpCode(200)
+  @ApiOperation({
+    summary: 'edit process by id',
+    description: 'for super/admin only',
+  })
+  @ApiBearerAuth('Authorization')
+  @ApiOkResponse({
+    description: 'Successfully edit process by id',
+    type: StatusResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: 'Validation Error',
+    type: ValidationErrorResponse,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Invalid Token or Expired',
+    type: UnauthorizedResponse,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden', type: ForbiddenResponse })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    type: InternalServerResponse,
+  })
   async editById(
     @Param('id') id: string,
     @Body() req: EditProcessDto,
@@ -67,6 +170,29 @@ export class ProcessController {
 
   @Patch('steps')
   @HttpCode(200)
+  @ApiOperation({
+    summary: 'edit process steps',
+    description: 'for super/admin only',
+  })
+  @ApiBearerAuth('Authorization')
+  @ApiBody({ type: [EditProcessStepDto] })
+  @ApiOkResponse({
+    description: 'Successfully edit process steps',
+    type: StatusResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: 'Validation Error',
+    type: ValidationErrorResponse,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Invalid Token or Expired',
+    type: UnauthorizedResponse,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden', type: ForbiddenResponse })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    type: InternalServerResponse,
+  })
   async editSteps(
     @Body() req: EditProcessStepDto[],
   ): Promise<StatusResponseDto> {
@@ -77,6 +203,28 @@ export class ProcessController {
 
   @Delete(':id')
   @HttpCode(200)
+  @ApiOperation({
+    summary: 'delete process by id',
+    description: 'for super/admin only',
+  })
+  @ApiBearerAuth('Authorization')
+  @ApiOkResponse({
+    description: 'Successfully detele process by id',
+    type: StatusResponseDto,
+  })
+  @ApiBadRequestResponse({
+    description: 'Validation Error',
+    type: ValidationErrorResponse,
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Invalid Token or Expired',
+    type: UnauthorizedResponse,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden', type: ForbiddenResponse })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+    type: InternalServerResponse,
+  })
   async deleteById(@Param('id') id: string): Promise<StatusResponseDto> {
     await this.processService.deleteById(id);
 
--- src/process/interface/http/process.request.ts
+++ src/process/interface/http/process.request.ts
@@ -1,18 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ProcessEntity } from 'src/process/domain/process.entity';
 import { v4 as uuid } from 'uuid';
 
 export class AddProcessDto {
+  @ApiProperty({ example: 'example' })
   name: string;
+
+  @ApiProperty({ example: 1 })
   step: number;
 }
 
 export class EditProcessDto {
+  @ApiProperty({ example: 'example' })
   name: string;
+
+  @ApiProperty({ example: 1 })
   step: number;
 }
 
 export class EditProcessStepDto {
+  @ApiProperty({ example: 'e6314752-c753-47dc-bc82-eae480d1b094' })
   id: string;
+
+  @ApiProperty({ example: 1 })
   step: number;
 }
 
