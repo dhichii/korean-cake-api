73e601d4556839344ea32a52b9d01a78f5e850f0
Adhicitta Masran <adhicittamasran@gmail.com>
2025-01-10 23:56:58 +0800

    feat: add pagination response dto

--- src/common/api-response.dto.ts
+++ src/common/api-response.dto.ts
@@ -17,6 +17,51 @@ export class StatusResponseDto {
   status: string = 'success';
 }
 
+export type PaginationPayload<T> = {
+  limit: number;
+  page: number;
+  totalResult: number;
+  data: T;
+};
+
+export class PaginationResponseDto<T> {
+  @ApiProperty({ description: 'Status of the response', example: 'success' })
+  readonly status: string = 'success';
+
+  @ApiProperty({ description: 'Limit of data showed per page', example: 5 })
+  readonly limit: number;
+
+  @ApiProperty({ description: 'Total of page', example: 2 })
+  readonly totalPage: number;
+
+  @ApiProperty({ description: 'Total of the data', example: 9 })
+  readonly totalResult: number;
+
+  @ApiProperty({ description: 'Current page', example: 1 })
+  readonly page: number;
+
+  @ApiProperty({ description: 'The data returned in the response' })
+  readonly data: T;
+
+  constructor(payload: PaginationPayload<T>) {
+    const { limit, page, totalResult, data } = payload;
+    this.limit = limit;
+    this.page = page;
+    this.totalPage = this.countTotalPage(totalResult, limit);
+    this.totalResult = totalResult;
+    this.data = data;
+  }
+
+  private countTotalPage(totalResult: number, limit: number) {
+    let total = Math.floor(totalResult / limit);
+    if (totalResult % limit > 0) {
+      total++;
+    }
+
+    return total;
+  }
+}
+
 export class ValidationErrorResponse {
   @ApiProperty({
     example: [
