08d35f2382ee2c85aa5716b6cee1543e39c6b8b3
Adhicitta Masran <adhicittamasran@gmail.com>
2025-01-12 17:31:01 +0800

    feat(order): add service interface

---
+++ src/order/domain/order.service.interface.ts
@@ -0,0 +1,29 @@
+import {
+  AddOrderDto,
+  EditOrderDto,
+  EditOrderProgressDto,
+  GetAllOrderDto,
+} from '../interface/http/order.request';
+import {
+  AddOrderResponseDto,
+  GetAllOrderResponseDto,
+  GetOrderByIdResponseDto,
+} from '../interface/http/order.response';
+
+export interface IOrderService {
+  add(
+    req: AddOrderDto,
+    userId: string,
+    pictures: Express.Multer.File[],
+  ): Promise<AddOrderResponseDto>;
+  getAll(req: GetAllOrderDto): Promise<[number, GetAllOrderResponseDto[]]>;
+  getById(id: string, userId: string): Promise<GetOrderByIdResponseDto>;
+  editById(id: string, userId: string, req: EditOrderDto): Promise<void>;
+  deleteById(id: string, userId: string): Promise<void>;
+  editProgressById(
+    id: string,
+    orderId: string,
+    userId: string,
+    req: EditOrderProgressDto,
+  ): Promise<void>;
+}
