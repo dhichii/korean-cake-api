fb16973e5be7d07d8973eed628e1cc9883447bba
Adhicitta Masran <adhicittamasran@gmail.com>
2025-01-15 22:50:39 +0800

    x

--- src/order/interface/http/order.controller.ts
+++ src/order/interface/http/order.controller.ts
@@ -50,7 +50,6 @@ import {
   getSchemaPath,
 } from '@nestjs/swagger';
 import { createFileFilter, MimeType } from '../../../utils/file-filter';
-import path from 'path';
 
 @Controller('/api/v1/orders')
 @UseGuards(JwtGuard)
@@ -107,7 +106,6 @@ export class OrderController {
   ): Promise<ApiResponseDto<AddOrderResponseDto>> {
     body.pictures = files['pictures'];
     console.log(body);
-    console.log(path.resolve(__dirname, './test/logo.png'));
     const data = await this.orderService.add(body, userId);
 
     return new ApiResponseDto(data);
