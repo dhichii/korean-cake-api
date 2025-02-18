9bae39527c6f924c4857785e096d1e39f6c67ac4
Adhicitta Masran <adhicittamasran@gmail.com>
2025-02-18 22:52:48 +0800

    build(prisma): add column userId to the authentications table

--- prisma/migrations/20241129165905_add_authentications_table/migration.sql
+++ prisma/migrations/20241129165905_add_authentications_table/migration.sql
@@ -1,8 +1,12 @@
 -- CreateTable
 CREATE TABLE "authentications" (
     "token" VARCHAR(500) NOT NULL,
+    "userId" TEXT NOT NULL,
     "expiresAt" TIMESTAMP(3) NOT NULL,
     "revoked" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "authentications_pkey" PRIMARY KEY ("token")
 );
+
+-- AddForeignKey
+ALTER TABLE "authentications" ADD CONSTRAINT "authentications_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
